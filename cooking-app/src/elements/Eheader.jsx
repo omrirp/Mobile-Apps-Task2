a18b6c6382dcd885b130c90e5221ca8da1381107
omrirp <omrirp@gmail.com>
2022-11-22 21:35:28 +0200

    header

--- cooking-app/src/elements/Eheader.jsx
+++ cooking-app/src/elements/Eheader.jsx
@@ -1,14 +1,7 @@
 const Eheader = (
     <header>
-        <h1 style={{ fontSize: 60, fontWeight: 'bold', textAlign: 'center' }}>
-            Cooking App
-        </h1>
-        <div className='menu' style={{ textAlight: 'center' }}>
-            <button>Add Ingrediant</button>
-            <button>Add Recipe</button>
-            <button>Show Ingredients</button>
-            <button>Show Recipes</button>
-        </div>
+        <h1 style={{ fontSize: 60, fontWeight: 'bold', textAlign: 'center' }}>My Kitchen</h1>
+        <div className="menu" style={{ textAlight: 'center' }}></div>
     </header>
 );
 
